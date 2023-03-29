6172e6fae43aefbf25c5dd2094a3b98c5823fcbc
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-03-30 01:33:09 +0200

    add react-i18next

--- my-app/src/components/header/index.jsx
+++ my-app/src/components/header/index.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
 
 
 
 const Header = () => {
     return (
       <div className="header">
-        
+        <nav className="nav">
+          <ul>
+            <li>   
+              <Link to="home_path">   </Link>
+            </li>
+            </ul>
+          </nav>
       </div>
     );
   };
