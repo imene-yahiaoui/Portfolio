415357525c0ef04e2f377563f41c4678ba6edc63
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-03-30 02:55:54 +0200

    add  i18n , useTranslation

--- my-app/src/components/header/index.jsx
+++ my-app/src/components/header/index.jsx
@@ -1,19 +1,35 @@
 import { Link } from "react-router-dom";
-
-
+import { useTranslation } from "react-i18next";
+import "./style.css";
 
 const Header = () => {
-    return (
-      <div className="header">
-        <nav className="nav">
-          <ul>
-            <li>   
-              <Link to="home_path">   </Link>
-            </li>
-            </ul>
-          </nav>
-      </div>
-    );
-  };
-  
-  export default Header;
+  // const { t, i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
+  return (
+    <div className="header">
+      <nav className="nav">
+        <ul>
+          <li className="test">{t("home_title")}</li>
+          <li>
+            <select>
+              <option value="fr" onClick={() => i18n.changeLanguage("fr")}>
+                {" "}
+                fr{" "}
+              </option>
+              <option value="en" onClick={() => i18n.changeLanguage("en")}>
+                {" "}
+                en{" "}
+              </option>
+              <option value="ar" onClick={() => i18n.changeLanguage("ar")}>
+                {" "}
+                ar{" "}
+              </option>
+            </select>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./i18n";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     <BrowserRouter>
-    <App />
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
