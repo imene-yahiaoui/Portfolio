22f961e7e489e8f5f109e390500865a7e9d9575e
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-04-01 16:31:34 +0200

    add Write effect

--- src/containers/intro/index.jsx
+++ src/containers/intro/index.jsx
@@ -6,8 +6,10 @@ const Intro = () => {
 
   return (
     <div className="intro-continer">
+     
       <h1> {t("main_heading.hello")} </h1>
-      <h1>{t("main_heading.name")} </h1>
+      <h1 className="intro-name" data-text={t("main_heading.name")}  >{t("main_heading.name")} </h1>
+   
       <div>
         <h3> {t("main_heading.static_desc")} </h3>
         <h3> {t("main_heading.dynamic_desc")} </h3>
