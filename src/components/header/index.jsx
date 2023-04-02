5e08a9fa3b2f4a60cd5874b85d8328cb9877ca74
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-04-02 02:54:41 +0200

    update css btn lang

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,118 +4,125 @@ import { useTranslation } from "react-i18next";
 import logo from "../../assets/images/logo.png";
 import "./style.css";
 import useMediaQuery from "../../helpers/MediaQuery";
-import {FaBars , FaTimes} from "react-icons/fa"
-import React, { useState } from 'react';
-import DarkMode from "../darkMode"
+import { FaBars, FaTimes } from "react-icons/fa";
+import React, { useState } from "react";
+import DarkMode from "../darkMode";
 const Header = () => {
   const matches = useMediaQuery("(max-width:767px)");
-  const { t ,i18n} = useTranslation();
- 
+  const { t, i18n } = useTranslation();
+
   const [showMenu, setShowMenu] = useState(false);
 
   const handleToggleMenu = () => {
     setShowMenu(!showMenu);
   };
- const handleToggleMenux=()=>{
-  setShowMenu(!showMenu);
- };
- console.log(showMenu)
+  const handleToggleMenux = () => {
+    setShowMenu(!showMenu);
+  };
+  console.log(showMenu);
   return (
     <div className="navbar">
       <nav className="container">
-<div className="logo">
-        <Link to="/">
-          <img  src={logo} alt={t("logo_alt")}></img>
-        </Link>
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt={t("logo_alt")}></img>
+          </Link>
         </div>
 
-
-        {!matches ?
-        <div className='nav-elements'>
-        <ul>
-          <li>
-            <Link to={t("home_path")}>{t("home_title")}</Link>
-          </li>
-          <li>
-            <Link to={t("skills_path")}>{t("skills_title")}</Link>
-          </li>
-          <li>
-            <Link to={t("portfolio_path")}>{t("portfolio_title")}</Link>
-          </li>
-          <li>
-            <Link to={t("contact_path")}>{t("contact_title")}</Link>
-          </li>
-          <li>
-            < DarkMode />
-            </li>
-          <li>
-            <select className="btn_lang">
-              <option value="fr" onClick={() => i18n.changeLanguage("fr")}>
-                {" "}
-                fr{" "}
-              </option>
-              <option value="en" onClick={() => i18n.changeLanguage("en")}>
-                {" "}
-                en{" "}
-              </option>
-              <option value="ar" onClick={() => i18n.changeLanguage("ar")}>
-                {" "}
-                ar{" "}
-              </option>
-            </select>
-          </li>
-        </ul>
-        </div>
-       :
-       //show if it's mobil 
-       <div className='navbar-header'> 
-       <div className='navbar-icon'>  
-     
-     {!showMenu ?
-     <button  onClick={handleToggleMenu}>
-          <FaBars  />
-        </button>:
-
-        <button    onClick={handleToggleMenux}> <FaTimes/>
-         </button>}
-
-       
-</div>
-        <div className={`navbar-menu${showMenu ? ' active' : ''}`}>
-        <ul className="nav-menu titles">
-          <li >
-            <Link to={t("home_path")}>{t("home_title")}</Link>
-          </li>
-          <li>
-            <Link to={t("skills_path")}>{t("skills_title")}</Link>
-          </li>
-          <li>
-            <Link to={t("portfolio_path")}>{t("portfolio_title")}</Link>
-          </li>
-          <li>
-            <Link to={t("contact_path")}>{t("contact_title")}</Link>
-          </li>
-          <li>
-            <select className="btn_lang">
-              <option value="fr" onClick={() => i18n.changeLanguage("fr")}>
-                {" "}
-                fr{" "}
-              </option>
-              <option value="en" onClick={() =>i18n.changeLanguage("en")}>
-                {" "}
-                en{" "}
-              </option>
-              <option value="ar" onClick={() => i18n.changeLanguage("ar") }>
-                {" "}
-                ar{" "}
-              </option>
-            </select>
-          </li>
-        </ul>
-        </div>
-       
-         </div>
-        }
+        {!matches ? (
+          <div className="nav-elements">
+            <ul>
+              <li>
+                <Link to={t("home_path")}>{t("home_title")}</Link>
+              </li>
+              <li>
+                <Link to={t("skills_path")}>{t("skills_title")}</Link>
+              </li>
+              <li>
+                <Link to={t("portfolio_path")}>{t("portfolio_title")}</Link>
+              </li>
+              <li>
+                <Link to={t("contact_path")}>{t("contact_title")}</Link>
+              </li>
+              <li>
+                <DarkMode />
+              </li>
+              <li>
+                <select className="btn_lang">
+                  <option value="fr" onClick={() => i18n.changeLanguage("fr")}>
+                    {" "}
+                    fr{" "}
+                  </option>
+                  <option value="en" onClick={() => i18n.changeLanguage("en")}>
+                    {" "}
+                    en{" "}
+                  </option>
+                  <option value="ar" onClick={() => i18n.changeLanguage("ar")}>
+                    {" "}
+                    ar{" "}
+                  </option>
+                </select>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          //show if it's mobil
+          <div className="navbar-header">
+            <div className="navbar-icon">
+              {!showMenu ? (
+                <button onClick={handleToggleMenu}>
+                  <FaBars />
+                </button>
+              ) : (
+                <button onClick={handleToggleMenux}>
+                  {" "}
+                  <FaTimes />
+                </button>
+              )}
+            </div>
+            <div className={`navbar-menu${showMenu ? " active" : ""}`}>
+              <ul className="nav-menu titles">
+                <li>
+                  <Link to={t("home_path")}>{t("home_title")}</Link>
+                </li>
+                <li>
+                  <Link to={t("skills_path")}>{t("skills_title")}</Link>
+                </li>
+                <li>
+                  <Link to={t("portfolio_path")}>{t("portfolio_title")}</Link>
+                </li>
+                <li>
+                  <Link to={t("contact_path")}>{t("contact_title")}</Link>
+                </li>
+                <li>
+                  <select className="btn_lang">
+                    <option
+                      value="fr"
+                      onClick={() => i18n.changeLanguage("fr")}
+                    >
+                      {" "}
+                      fr{" "}
+                    </option>
+                    <option
+                      value="en"
+                      onClick={() => i18n.changeLanguage("en")}
+                    >
+                      {" "}
+                      en{" "}
+                    </option>
+                    <option
+                      value="ar"
+                      onClick={() => i18n.changeLanguage("ar")}
+                    >
+                      {" "}
+                      ar{" "}
+                    </option>
+                  </select>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </nav>
     </div>
   );
