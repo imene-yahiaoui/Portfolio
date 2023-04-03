c150790b081c3806123e9f0b439efc1f91a2857f
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-04-04 00:33:32 +0200

    update SocialMedia buttons

--- src/components/socialMedia/index.jsx
+++ src/components/socialMedia/index.jsx
@@ -1,21 +1,60 @@
-import {DiGithubBadge } from   "react-icons/di";
-import {FaDiscord } from "react-icons/fa";
-import {TiSocialLinkedinCircular } from "react-icons/ti";
-import "./style.css"
+import { DiGithubBadge } from "react-icons/di";
+import { FaDiscord } from "react-icons/fa";
+import { TiSocialLinkedinCircular } from "react-icons/ti";
+import "./style.css";
 const SocialMedia = () => {
-  return( <ul className="socialMedia">
- 
-<li>
- <a  href ="https://github.com/imene-yahiaoui" target="_blank" rel="noreferrer" ><DiGithubBadge  color="var(--body_color)"size="50px"/></a>
-</li>
-<li>
- <a href ="https://github.com/imene-yahiaoui" target="_blank" rel="noreferrer" ><FaDiscord  color="var(--body_color)" size="40px"/></a>
- </li>
- <li>
- <a href ="https://github.com/imene-yahiaoui" target="_blank" rel="noreferrer" ><TiSocialLinkedinCircular  color="var(--body_color)" size="50px"/></a>
- </li>
-  </ul>
-  )
+  return (
+    <ul className="socialMedia">
+      <li>
+        <a
+          href="https://github.com/imene-yahiaoui"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <DiGithubBadge
+            color="var(--body_color)"
+            size="50px"
+            onMouseOver={({ target }) =>
+              (target.style.color = "var(--lang_color)")
+            }
+            onMouseOut={({ target }) =>
+              (target.style.color = "var(--body_color)")
+            }
+          />
+        </a>
+      </li>
+      <li>
+        <a href="https://discord.gg/#1420" target="_blank" rel="noreferrer">
+          <FaDiscord color="var(--body_color)" size="40px" 
+           onMouseOver={({ target }) =>
+           (target.style.color = "var(--lang_color)")
+         }
+         onMouseOut={({ target }) =>
+           (target.style.color = "var(--body_color)")
+         }
+          
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/imene-ikhlef-yahiaoui"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <TiSocialLinkedinCircular  color="var(--body_color)" size="50px" 
+           onMouseOver={({ target }) =>
+           (target.style.color = "var(--lang_color)")
+         }
+         onMouseOut={({ target }) =>
+           (target.style.color = "var(--body_color)")
+         }
+          
+          />
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 export default SocialMedia;
