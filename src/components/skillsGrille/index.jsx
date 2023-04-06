9ec994eac31d6aaca3d5e5fa0f73095d10aee1c0
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-04-07 01:38:10 +0200

    add name to skills

--- src/components/skillsGrille/index.jsx
+++ src/components/skillsGrille/index.jsx
@@ -32,115 +32,133 @@ const SkillsGrille = () => {
   const skillsCategory = [
     {
       image: html,
-      alt: "html icon  ",
+      alt: "html icon ",
+      name:"Html",
       category: "Frontend",
       key: "1",
     },
     {
       image: css,
       alt: " icon css",
+      name:"Css",
       category: "Frontend",
       key: "2",
     },
     {
       image: admobe,
       alt: " icon admobe",
+      name:"Admob",
       category: "Logiciel",
       key: "3",
     },
     {
       image: github,
       alt: "icon github",
+      name:"Github",
       category: "Logiciel",
       key: "4",
     },
     {
       image: figma,
       alt: " icon figma",
+      name:"Figma",
       category: "Frontend",
       key: "5",
     },
     {
       image: express,
       alt: "icon express",
+      name:"Express",
       category: "Backend",
       key: "6",
     },
     {
       image: js,
       alt: "icon js",
+      name:"JavaScript",
       category: "Frontend",
       key: "7",
     },
     {
       image: mongodb,
       alt: "icon mongodb",
+      name:"Mongodb",
       category: "Backend",
       key: "8",
     },
     {
       image: node,
       alt: "icon node.js",
+      name:"Node.js",
       category: "Backend",
       key: "9",
     },
     {
       image: bootstrap,
       alt: "icon bootstrap",
+      name:"Bootstrap",
       category: "Frontend",
       key: "10",
     },
     {
       image: kotlin,
       alt: " icon Kotlin",
+      name:"Kotlin",
       category: "Mobile",
       key: "11",
     },
     {
       image: android,
       alt: "icon android stodio",
+      name:"Android Studio",
       category: "Logiciel",
       key: "12",
     },
     {
       image: postman,
       alt: "icon postman",
+      name:"Postman",
       category: "Logiciel",
       key: "13",
     },
     {
       image: react,
-      alt: "icon react ",
+      alt: "React ",
       category: "Frontend",
       key: "14",
     },
     {
       image: sass,
       alt: "icon sass",
+      name:"Sass",
       category: "Frontend",
       key: "15",
     },
     {
       image: git,
       alt: "icon git",
+      name:"Git",
       category: "Logiciel",
       key: "16",
     },
     {
       image: VS,
       alt: "icon vs",
+      name:"Visual Studio",
       category: "Logiciel",
       key: "17",
     },
     {
       image: seo,
       alt: "icon seo",
+      name:"Seo",
       category: "Logiciel",
       key: "18",
     },
     {
       image: redux,
       alt: "icon redux",
+      name:"Redux",
       category: "Frontend",
       key: "19",
     },
@@ -151,6 +169,7 @@ const SkillsGrille = () => {
       {skillsCategory.map((skill) => (
         <div className="skillsGrille_item" key={skill.key}>
           <img src={skill.image} alt={skill.alt} />
+          <b>{skill.name}</b>
         </div>
       ))}
     </div>
@@ -161,6 +180,7 @@ const SkillsGrille = () => {
         .map((skill) => (
           <div className="skillsGrille_item" key={skill.key}>
             <img src={skill.image} alt={skill.alt} />
+            <b>{skill.name}</b>
           </div>
         ))}
     </div>
