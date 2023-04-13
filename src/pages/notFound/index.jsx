400b1fa843bb65e76ffda2527a15e7636450feee
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-04-14 01:28:43 +0200

    add animation page not found 404

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,15 +1,45 @@
-import { Link } from "react-router-dom";
 import "./style.css";
+import { useTranslation } from "react-i18next";
 
 const NotFound = () => {
+  const { t } = useTranslation();
   return (
-    <div className="notFound">
-      <p className="notFound-p">
-        Oups! La page que <span>vous demandez n'existe pas.</span>
-      </p>
-      <Link to="/">Retourner sur la page d'accueil</Link>
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+    <div>
+      <header className="top-header"></header>
+
+      <div>
+        <div className="starsec"></div>
+        <div className="starthird"></div>
+        <div className="starfourth"></div>
+        <div className="starfifth"></div>
+      </div>
+
+      <div className="lamp__wrap">
+        <div className="lamp">
+          <div className="cable"></div>
+          <div className="cover"></div>
+          <div className="in-cover">
+            <div className="bulb"></div>
+          </div>
+          <div className="light"></div>
+        </div>
+      </div>
+
+      <section className="error">
+        <div className="error__content">
+          <div className="error__message message">
+            <h1 className="message__title">{t("notFound.message__title")}</h1>
+            <p className="message__text">{t("notFound.message__text")}</p>
+          </div>
+          <div className="error__nav e-nav">
+            <a href="/" className="e-nav__link">
+              {t("notFound.message__btn")}
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
-
 export default NotFound;
