2fd82fdfc6d01e4c0e3458d972f6fb6dda67e8c6
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-04-18 01:43:29 +0200

    update intro

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import RoutesPath from "./containers/RoutesPath";
 
-// import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-
+ 
 function App() {
   return (
     <div className="App">
-      {/* <Parallax page={3} style={{ backgroundImage: "var(--body_background)" }}> */}
-      {/* <ParallaxLayer offset={0}></ParallaxLayer>
-        <ParallaxLayer offset={1}> </ParallaxLayer>
-        <ParallaxLayer offset={2}> </ParallaxLayer> */}
+    
       <Header />
       <RoutesPath />
       <Footer />
-      {/* </Parallax> */}
+     
     </div>
   );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,16 +3,22 @@ import Accueil from "../../containers/accueil";
 import Skills from "../../containers/skills";
 import Portfolio from "../../containers/portfolio";
 import { useTranslation } from "react-i18next";
-
+ 
 const Home = () => {
   const { t } = useTranslation();
   return (
     <div className="home">
-      <Accueil id={t("home_path")} />
-
-      <Skills />
-
-      <Portfolio />
+     
+          <Accueil id={t("home_path")} />
+      
+      
+          <Skills />
+      
+      
+        
+          <Portfolio />
+     
+     
     </div>
   );
 };
