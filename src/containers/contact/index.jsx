328d8fbf8f836afa0a083d36e8c00482c1650f4d
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-04-20 01:04:32 +0200

    add emailjs

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,24 +1,42 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./style.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ftu8pss",
+        "template_8chj4e7",
+        form.current,
+        "UBZTusyE5gpEpliK1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="container_contact">
-       <form name="contact" method="post">
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>,
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
 };
-
 export default Contact;
