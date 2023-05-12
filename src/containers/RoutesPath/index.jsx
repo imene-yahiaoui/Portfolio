e9f2801cd0fc6903de3b1976d1e2b9d11cd8ed32
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-05-12 23:50:22 +0200

    add BrowserRouter

--- src/containers/RoutesPath/index.jsx
+++ src/containers/RoutesPath/index.jsx
@@ -2,17 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../../pages/home";
 import NotFound from "../../pages/notFound";
 import Project from "../../pages/project";
-import { BrowserRouter} from "react-router-dom";
+
 const RoutesPath = () => {
   return (
     <div>
-        <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/Project/:id" element={<Project />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
-      </BrowserRouter>
     </div>
   );
 };
