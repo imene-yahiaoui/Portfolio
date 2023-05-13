fb3ce0e97d61721c5906edf06cbcaa2324c725a3
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-05-13 21:41:37 +0200

    Add responsive images for mobile

--- src/containers/accueil/index.jsx
+++ src/containers/accueil/index.jsx
@@ -1,16 +1,17 @@
 import "./style.min.css";
 import Intro from "../intro";
 import photoImene from "../../assets/images/imeneYahiaoui.webp";
+import photoImeneMobile from "../../assets/images/imeneikhlef.webp";
 import { useTranslation } from "react-i18next";
 // import Particle from "../../helpers/Particle";
-// import useMediaQuery from "../../helpers/MediaQuery";
+ import useMediaQuery from "../../helpers/MediaQuery";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 const Accueil = () => {
   const { t } = useTranslation();
   const lang = localStorage.getItem("i18nextLng");
-  // const matches = useMediaQuery("(max-width:767px)");
+ const matches = useMediaQuery("(max-width:767px)");
   useEffect(() => {
     AOS.init({ duration: 2000 });
   }, []);
@@ -28,7 +29,11 @@ const Accueil = () => {
         <Intro />
 
         <div className="container_logo" data-aos="zoom-in-up">
+        {!matches ? 
           <img src={photoImene} alt={t("photo_alt")} />
+           :
+          <img src={photoImeneMobile} alt={t("photo_alt")} />
+        }
         </div>
       </div>
     </div>
