9b6db9c1326d3ed9858a3d7594a43cda468ba37e
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-05-15 09:24:58 +0200

    add sitrmap.xml

--- src/containers/accueil/index.jsx
+++ src/containers/accueil/index.jsx
@@ -3,25 +3,19 @@ import Intro from "../intro";
 import photoImene from "../../assets/images/imeneYahiaoui.webp";
 import photoImeneMobile from "../../assets/images/imeneikhlef.webp";
 import { useTranslation } from "react-i18next";
-// import Particle from "../../helpers/Particle";
- import useMediaQuery from "../../helpers/MediaQuery";
+import useMediaQuery from "../../helpers/MediaQuery";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 const Accueil = () => {
   const { t } = useTranslation();
   const lang = localStorage.getItem("i18nextLng");
- const matches = useMediaQuery("(max-width:767px)");
+  const matches = useMediaQuery("(max-width:767px)");
   useEffect(() => {
     AOS.init({ duration: 2000 });
   }, []);
   return (
     <div id={t("home_id")}>
-      {/* {!matches ? (
-        <div className="particles-container">
-          <Particle />
-        </div>
-      ) : null} */}
       <div
         style={{ flexDirection: lang === "ar" ? "row-reverse" : "row" }}
         className="container accueil"
@@ -29,11 +23,11 @@ const Accueil = () => {
         <Intro />
 
         <div className="container_logo" data-aos="zoom-in-up">
-        {!matches ? 
-          <img src={photoImene} alt={t("photo_alt")} />
-           :
-          <img src={photoImeneMobile} alt={t("photo_alt")} />
-        }
+          {!matches ? (
+            <img src={photoImene} alt={t("photo_alt")} />
+          ) : (
+            <img src={photoImeneMobile} alt={t("photo_alt")} />
+          )}
         </div>
       </div>
     </div>
