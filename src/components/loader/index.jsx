f77163332fe215f6064f34ab5d4981bf73ee3fce
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-07-12 00:40:55 +0200

    update  Loader

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -2,14 +2,15 @@ import "./style.min.css";
 
 const Loader = () => {
   return (
-    <div className="loader-page-container">
-      <div className="loader-container">
-        <div className="loader-wrap">
-          <div className="loader loader-2"></div>
-        </div>
-        <p className="code-block loading-text">Loading....</p>
-      </div>
+    <div className="pendulum">
+    <div className="pendulum_box">
+      <div className="ball first"></div>
+        <div className="ball"></div>
+        <div className="ball"></div>
+        <div className="ball"></div>
+        <div className="ball last"></div>
     </div>
+  </div>
   );
 };
 
