b1c311d334a9050e7005ef10fe88b4cc9b7fd8b5
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-09-22 18:26:32 +0200

    include keywords for better project organization

--- src/App.js
+++ src/App.js
@@ -2,8 +2,33 @@ import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import RoutesPath from "./containers/RoutesPath";
+import { useState, useEffect } from "react";
+import DisplayMessage from "./components/displayMessage";
 
 function App() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    // Afficher un message si l'utilisateur est hors ligne
+    if (!isOnline) {
+      DisplayMessage(
+        "Vous êtes hors ligne. Certaines fonctionnalités peuvent ne pas être disponibles",
+        "linear-gradient(to right, #b90909, #b90908)"
+      );
+    }
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, [isOnline]);
+
   return (
     <div className="App">
       <Header />
